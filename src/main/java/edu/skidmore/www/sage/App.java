1ead7914c0e10b6b439460f403dd5ba4194cab04
Giorgos Petkakis <giorgos@giorgos-GL502VS>
2017-06-09 05:55:38 -0400

    Initial commit

---
+++ src/main/java/edu/skidmore/www/sage/App.java
@@ -0,0 +1,29 @@
+package edu.skidmore.www.sage;
+
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Model m = ModelFactory.createDefaultModel();
+        String NS = "http://example/com/test/";
+        
+        Resource r = m.createResource( NS + "r" );
+        Property p = m.createProperty( NS + "p" );
+        
+        r.addProperty(p, "hello world", XSDDatatype.XSDstring);
+        
+        m.write( System.out, "Turtle");
+    }
+    
+
+}
